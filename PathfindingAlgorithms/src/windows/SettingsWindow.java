18ff97cdf7540c4b8eceb6167a4eff34cf60b92f
tzada8 <tzada@uwaterloo.ca>
2021-04-07 19:38:42 -0400

    Added sections for SizesAndLocation settings

--- PathfindingAlgorithms/src/windows/SettingsWindow.java
+++ PathfindingAlgorithms/src/windows/SettingsWindow.java
@@ -1,7 +1,5 @@
 package windows;
 
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -10,10 +8,9 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 
 import main.PathfindingMain;
+import settingsSections.SizesAndLocation;
 
 public class SettingsWindow extends JFrame implements ActionListener {
     
@@ -24,8 +21,7 @@ public class SettingsWindow extends JFrame implements ActionListener {
     
     // Fields
     private JButton finish; // submit all settings
-    private JTextField widthField;
-    private JTextField heightField;
+    private SizesAndLocation sizesAndLocation = new SizesAndLocation();
 
     public SettingsWindow() {
 	// Settings Window properties
@@ -40,30 +36,8 @@ public class SettingsWindow extends JFrame implements ActionListener {
 	finish.setFont(new Font(PathfindingMain.GAME_FONT, Font.PLAIN, 16));
 	finish.addActionListener(this);
 
-	// Labels for maze size (width and height)
-	JLabel heightLabel = new JLabel("Height:");
-	JLabel widthLabel = new JLabel("Width:");
-	
-	// Textfields for maze size (width and height)
-	heightField = new JTextField("Height");
-	heightField.setPreferredSize(new Dimension(100, 20));
-	heightField.setFont(new Font(PathfindingMain.GAME_FONT, Font.PLAIN, 16));
-	heightField.setForeground(new Color(0x00FF00));
-	heightField.setBackground(new Color(255, 0, 0));
-	heightField.setCaretColor(Color.white);
-	
-	widthField = new JTextField("Width");
-	widthField.setPreferredSize(new Dimension(100, 20));
-	widthField.setFont(new Font(PathfindingMain.GAME_FONT, Font.PLAIN, 16));
-	widthField.setForeground(new Color(0x00FF00));
-	widthField.setBackground(new Color(255, 0, 0));
-	widthField.setCaretColor(Color.white);
-
 	// Adding all elements
-	this.add(heightLabel);
-	this.add(heightField);
-	this.add(widthLabel);
-	this.add(widthField);
+	this.add(sizesAndLocation);
 	this.add(finish);
 	
 	this.pack();
@@ -75,19 +49,9 @@ public class SettingsWindow extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 	if (e.getSource() == finish) {
-	    System.out.println("Hello " + widthField.getText());
+	    System.out.println("Hello " + sizesAndLocation.getWidthField());
+	    System.out.println("Hello " + sizesAndLocation.getHeightField());
 	    this.dispose();
 	}
     }
-    
-    // Instance method that returns the value the user set for the WIDTH field
-    public String getWidthField() {
-	return widthField.getText();
-    }
-    
-    // Instance method that returns the value the user set for the HEIGHT field
-    public String getHeightField() {
-	return heightField.getText();
-    }
-
 }
