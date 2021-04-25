95ae1dae94cfbc0d1ced679e629de914e387dfa1
tzada8 <tzada@uwaterloo.ca>
2021-04-24 22:35:21 -0400

    Updated comments for GridPanel and Node classes

--- PathfindingAlgorithms/src/sections/board/GridPanel.java
+++ PathfindingAlgorithms/src/sections/board/GridPanel.java
@@ -11,6 +11,19 @@ import sections.board.presets.MakePreset2;
 import sections.board.presets.MakePreset3;
 import sections.board.presets.MakeRandom;
 
+/**
+ * 
+ * The following GridPanel class extends JPanel and acts as a container for
+ * every Node that has been created. This class stores all the Nodes in a 2D
+ * array allowing each Node to be accessed when necessary.
+ * 
+ * All methods of this class are for the grid as an entirety, applying to all
+ * Nodes.
+ * 
+ * @author Troy Zada
+ *
+ */
+
 public class GridPanel extends JPanel {
 
     /**
@@ -31,6 +44,10 @@ public class GridPanel extends JPanel {
     static boolean clickable = true;
 
     // Basic Panel that holds all content for a given algorithm
+    /**
+     * This constructor creates the panel for the grid combining each individual
+     * Node to make the entire grid.
+     */
     public GridPanel() {
 	this.setPreferredSize(new Dimension(MAP_SIZE_WIDTH, MAP_SIZE_HEIGHT));
 	this.setBackground(null);
@@ -45,12 +62,22 @@ public class GridPanel extends JPanel {
 	}
     }
 
-    // Gets Node at specified parameters
+    /**
+     * Getter method to get the Node that sits at the given row and column.
+     * 
+     * @param row - Row of the Node being returned.
+     * @param col - Column of the Node being returned.
+     * @return - The Node sitting at the specified row and column.
+     */
     public Node getNode(int row, int col) {
 	return map[row][col];
     }
 
-    // Gets Start Node from map
+    /**
+     * Goes through entire board looking for the start Node (ORANGE).
+     * 
+     * @return - Start Node if one exists, and null otherwise.
+     */
     public Node getStartNode() {
 	for (int r = 0; r < ROWS; r++) {
 	    for (int c = 0; c < COLUMNS; c++) {
@@ -62,7 +89,11 @@ public class GridPanel extends JPanel {
 	return null;
     }
 
-    // Gets End Node from map
+    /**
+     * Goes through entire board looking for the end Node (CYAN).
+     * 
+     * @return - End Node if one exists, and null otherwise.
+     */
     public Node getEndNode() {
 	for (int r = 0; r < ROWS; r++) {
 	    for (int c = 0; c < COLUMNS; c++) {
@@ -74,8 +105,12 @@ public class GridPanel extends JPanel {
 	return null;
     }
 
-    /*
-     * Returns all the adjacent Nodes to Node v that are available (WHITE Nodes).
+    /**
+     * Gets all the Nodes that are adjacent to the parameter Node, v, that are
+     * available (WHITE).
+     * 
+     * @param v - The previous Node.
+     * @return - All Nodes adjacent to Node v.
      */
     public Set<Node> getAdjacencyNodes(Node v) {
 
@@ -92,29 +127,30 @@ public class GridPanel extends JPanel {
 	int bottomR = determineBoundaries(currentRow + 1, GridPanel.ROWS);
 	int leftC = determineBoundaries(currentCol - 1, GridPanel.COLUMNS);
 
-	// TOP
-	addAdjacentNodes(adjacentNodes, v, topR, currentCol);
-	// RIGHT
-	addAdjacentNodes(adjacentNodes, v, currentRow, rightC);
-	// BOTTOM
-	addAdjacentNodes(adjacentNodes, v, bottomR, currentCol);
-	// LEFT
+	addAdjacentNodes(adjacentNodes, v, topR, currentCol); // TOP
+	addAdjacentNodes(adjacentNodes, v, currentRow, rightC); // RIGHT
+	addAdjacentNodes(adjacentNodes, v, bottomR, currentCol); // BOTTOM
+	addAdjacentNodes(adjacentNodes, v, currentRow, leftC); // LEFT
 
-	addAdjacentNodes(adjacentNodes, v, currentRow, leftC);
-	// TOP-LEFT
-	addAdjacentNodes(adjacentNodes, v, topR, leftC);
-	// TOP-RIGHT
-	addAdjacentNodes(adjacentNodes, v, topR, rightC);
-	// BOTTOM-RIGHT
-	addAdjacentNodes(adjacentNodes, v, bottomR, rightC);
-	// BOTTOM-LEFT
-	addAdjacentNodes(adjacentNodes, v, bottomR, leftC);
+	addAdjacentNodes(adjacentNodes, v, topR, leftC); // TOP-LEFT
+	addAdjacentNodes(adjacentNodes, v, topR, rightC); // TOP-RIGHT
+	addAdjacentNodes(adjacentNodes, v, bottomR, rightC); // BOTTOM-RIGHT
+	addAdjacentNodes(adjacentNodes, v, bottomR, leftC); // BOTTOM-LEFT
 
 	return adjacentNodes;
     }
 
-    // General method to determine if a Node outside the board is trying to be
-    // accessed
+    /**
+     * Private helper method to determine if a Node outside the board is trying to
+     * be accessed, being a negative index or greater than the number of ROWS /
+     * COLUMNS.
+     * 
+     * @param testVal             - The location being tests to see if it lies
+     *                            within the boundaries.
+     * @param upperLimitingFactor - Factor for upper limit (total ROWS / COLUMNS).
+     * @return - The corrected location such that value will still lie within the
+     *         boundaires.
+     */
     private int determineBoundaries(int testVal, int upperLimitingFactor) {
 	// If outside lower or upper boundaries, adjust value to keep it inside
 	if (testVal < 0) {
@@ -125,7 +161,15 @@ public class GridPanel extends JPanel {
 	return testVal;
     }
 
-    // Helper method to determine if given point should be added to adjacency list
+    /**
+     * Private helper method to determine if a given point should be added to the
+     * adjacency set or not.
+     * 
+     * @param adjacency - Set for all Nodes adjacent to the current one.
+     * @param current   - The current Node.
+     * @param rowLoc    - The given row for a potential adjacent Node.
+     * @param colLoc    - The given column for a potential adjacent Node.
+     */
     private void addAdjacentNodes(Set<Node> adjacency, Node current, int rowLoc, int colLoc) {
 	if ((map[rowLoc][colLoc].isAvailable() || map[rowLoc][colLoc].isEnd())
 		&& !map[rowLoc][colLoc].equals(current)) {
@@ -133,6 +177,9 @@ public class GridPanel extends JPanel {
 	}
     }
 
+    /**
+     * Resets all costs of every Node back to default value of the double max value.
+     */
     public void resetAllCosts() {
 	for (int r = 0; r < GridPanel.ROWS; r++) {
 	    for (int c = 0; c < GridPanel.COLUMNS; c++) {
@@ -141,7 +188,10 @@ public class GridPanel extends JPanel {
 	}
     }
 
-    // Goes through entire board and resets each Node to colour WHITE
+    /**
+     * Goes through entire board and resets each Node to a colour of WHITE, as well
+     * as stating that the start and end points haven't been placed anymore.
+     */
     private void resetBoard() {
 	for (int r = 0; r < ROWS; r++) {
 	    for (int c = 0; c < COLUMNS; c++) {
@@ -154,8 +204,10 @@ public class GridPanel extends JPanel {
 	endPointPlaced = false;
     }
 
-    // Goes through entire board and resets pathfinding Nodes to WHITE
-    // --> RED, GREEN, and MAGENTA
+    /**
+     * Goes through entire board and resets pathfinding Nodes to a colour of WHITE.
+     * This includes all RED, GREEN, and MAGENTA Nodes.
+     */
     public void resetPathfinding() {
 	for (int r = 0; r < ROWS; r++) {
 	    for (int c = 0; c < COLUMNS; c++) {
@@ -166,7 +218,12 @@ public class GridPanel extends JPanel {
 	}
     }
 
-    // Goes through board and verifies if there is both a START and END point
+    /**
+     * Goes through the entire board and verifies that both a start and end point
+     * have been place.
+     * 
+     * @return - Whether both start and end point exist.
+     */
     public boolean hasStartAndEndPoint() {
 	int counter = 0;
 	for (int r = 0; r < ROWS; r++) {
@@ -179,52 +236,70 @@ public class GridPanel extends JPanel {
 	return counter == 2;
     }
 
-    // Makes all Node's on the board unclickable
+    /**
+     * Makes all Node's on the board unclickable.
+     */
     public void makeUnclickable() {
 	clickable = false;
     }
 
-    // Makes all Node's on the board clickable
+    /**
+     * Makes all Node's on the board clickable.
+     */
     public void makeClickable() {
 	clickable = true;
     }
 
-    // Makes board both unclickable and clears all Nodes to WHITE
+    /**
+     * Makes the board unclickable and resets all Nodes to WHITE.
+     */
     private void unclickableAndEmptyBoard() {
 	this.resetBoard();
 	this.makeUnclickable();
     }
 
-    // Makes board both clickable and clears all Nodes to WHITE
+    /**
+     * Makes the board clickable and resets all Nodes to WHITE.
+     */
     private void clickableAndEmptyBoard() {
 	this.resetBoard();
 	this.makeClickable();
     }
 
-    // Creates the Freehand board
+    /**
+     * Creates a Freehand board.
+     */
     public void makeFreehand() {
 	this.clickableAndEmptyBoard();
     }
 
-    // Creates the Preset1 board
+    /**
+     * Creates the Preset1 board by calling the MakePreset1 class.
+     */
     public void makePreset1() {
 	this.unclickableAndEmptyBoard();
 	new MakePreset1(map);
     }
 
-    // Creates the Preset2 board
+    /**
+     * Creates the Preset2 board by calling the MakePreset2 class.
+     */
     public void makePreset2() {
 	this.unclickableAndEmptyBoard();
 	new MakePreset2(map);
     }
 
-    // Creates the Preset3 board
+    /**
+     * Creates the Preset3 board by calling the MakePreset3 class.
+     */
     public void makePreset3() {
 	this.unclickableAndEmptyBoard();
 	new MakePreset3(map);
     }
 
-    // Creates the Random board
+    /**
+     * Creates a Random board by calling the MakeRandom class.
+     */
     public void makeRandom() {
 	this.unclickableAndEmptyBoard();
 	new MakeRandom(map);
--- PathfindingAlgorithms/src/sections/board/Node.java
+++ PathfindingAlgorithms/src/sections/board/Node.java
@@ -7,12 +7,24 @@ import java.awt.event.MouseListener;
 import javax.swing.JLabel;
 import javax.swing.border.MatteBorder;
 
+/**
+ * 
+ * The following Node class extends JLabel and implements a MouseListener acting
+ * as a box for each individual cell / tile. Each Node has a specific row,
+ * column, and colour, as well as g, h, and f cost values for the A* algorithm.
+ * 
+ * The MouseListener allows the Nodes to be clicked on in order to change it
+ * colour for the different specifications.
+ * 
+ * Nodes can either be open (WHITE), a barrier (BLACK), a start point (ORANGE),
+ * an end point (CYAN), a closed path (RED), an open path (GREEN), or the
+ * solution (MAGENTA).
+ * 
+ * @author Troy Zada
+ *
+ */
+
 public class Node extends JLabel implements MouseListener {
-    /*
-     * Nodes can either be open (WHITE), an obstacle (BLACK), a start point
-     * (ORANGE), an end point (CYAN), a closed path (RED), an open path (GREEN), or
-     * the solution (MAGENTA)
-     */
 
     /**
      * Default Serialization
@@ -28,13 +40,19 @@ public class Node extends JLabel implements MouseListener {
     private Color colour;
     private double gCost, hCost, fCost = Double.MAX_VALUE;
 
-    // Node object with WHITE colour
+    /**
+     * This constructor creates a Node object at the specified row and column,
+     * adding a border and initializing it as WHITE.
+     * 
+     * @param row - The row this Node sits at.
+     * @param col - The column this Node sits at.
+     */
     public Node(int row, int col) {
 	this.setBounds(col * NODE_SIZE, row * NODE_SIZE, NODE_SIZE, NODE_SIZE);
 	this.setOpaque(true);
 	this.addMouseListener(this);
 
-	// Add border to Nodes --> (TOP, LEFT, BOTTOM, RIGHT, COLOUR)
+	// Add border to Nodes, parameter order is: TOP, LEFT, BOTTOM, RIGHT, COLOUR
 	if (row == 0 && col == 0) { // Top-left corner
 	    this.setBorder(new MatteBorder(2, 2, 0, 1, Color.BLACK));
 	} else if (row == 0 && col == GridPanel.COLUMNS - 1) { // Top-right corner
@@ -59,139 +77,235 @@ public class Node extends JLabel implements MouseListener {
 	this.makeAvailable();
     }
 
-    // Get position of this Node
+    /**
+     * Getter method to get the position of this Node.
+     * 
+     * @return - This Node's position, being row and col.
+     */
     public int[] getPosition() {
 	int[] pos = { this.row, this.col };
 	return pos;
     }
 
-    // Get colour of Node
+    /**
+     * Getter method to get the colour of this Node.
+     * 
+     * @return - This Node's colour.
+     */
     public Color getColour() {
 	return colour;
     }
 
-    // Checks if 2 Nodes are equal, meaning same position and colour
+    /**
+     * Determines if 2 Nodes are equal to one another, meaning same position (row
+     * and col) and colour.
+     * 
+     * @param other - Node being compared to this.
+     * @return - True / False stating if this and other Nodes are equal or not.
+     */
     public boolean equals(Node other) {
 	return this.row == other.row && this.col == other.col && this.colour.equals(other.colour);
     }
 
-    /****** FOR CREATING MAP ******/
-    // Colour is WHITE, meaning it's available
+    /* FOR CREATING THE MAP */
+    /**
+     * If Node's colour is WHITE, then it's available.
+     * 
+     * @return - If Node is available.
+     */
     public boolean isAvailable() {
 	return colour == Color.WHITE;
     }
 
-    // Make Node available (WHITE)
+    /**
+     * Makes this Node available by changing its colour to WHITE.
+     */
     public void makeAvailable() {
 	colour = Color.WHITE;
 	this.setBackground(Color.WHITE);
     }
 
-    // Colour is BLACK, meaning it's an obstacle/barrier
+    /**
+     * If Node's colour is BLACK, then it's a barrier.
+     * 
+     * @return - If Node is a barrier.
+     */
     public boolean isBarrier() {
 	return colour == Color.BLACK;
     }
 
-    // Make Node an obstacle/barrier (BLACK)
+    /**
+     * Makes this Node a barrier by changing its colour to BLACK.
+     */
     public void makeBarrier() {
 	colour = Color.BLACK;
 	this.setBackground(Color.BLACK);
     }
 
-    // Colour is ORANGE, meaning it's start point
+    /**
+     * If Node's colour is ORANGE, then it's start point.
+     * 
+     * @return - If Node is start point.
+     */
     public boolean isStart() {
 	return colour == Color.ORANGE;
     }
 
-    // Make Node start point (ORANGE)
+    /**
+     * Makes this Node a start point by changing its colour to ORANGE.
+     */
     public void makeStart() {
 	colour = Color.ORANGE;
 	this.setBackground(Color.ORANGE);
     }
 
-    // Colour is CYAN, meaning it's end point
+    /**
+     * If Node's colour is CYAN, then it's end point.
+     * 
+     * @return - If Node is end point.
+     */
     public boolean isEnd() {
 	return colour == Color.CYAN;
     }
 
-    // Make Node end point (CYAN)
+    /**
+     * Makes this Node an end point by changing its colour to CYAN.
+     */
     public void makeEnd() {
 	colour = Color.CYAN;
 	this.setBackground(Color.CYAN);
     }
+    /* FOR CREATING THE MAP */
 
-    /****** FOR PATHFINDING ******/
-    // Colour is RED, meaning it has been checked
+    /* FOR PATHFINDING THROUGH THE MAP */
+    /**
+     * If Node's colour is RED, then it has been checked.
+     * 
+     * @return - If Node has already been checked.
+     */
     public boolean isClosed() {
 	return colour == Color.RED;
     }
 
-    // Make Node checked (RED)
+    /**
+     * Makes this Node closed by changing its colour to RED.
+     */
     public void makeClosed() {
 	colour = Color.RED;
 	this.setBackground(Color.RED);
     }
 
-    // Colour is GREEN, meaning it's available to be checked
+    /**
+     * If Node's colour is GREEN, then it can be checked.
+     * 
+     * @return - If Node is available to be checked.
+     */
     public boolean isOpen() {
 	return colour == Color.GREEN;
     }
 
-    // Make Node open to be checked (GREEN)
+    /**
+     * Makes this Node open by changing its colour to GREEN.
+     */
     public void makeOpen() {
 	colour = Color.GREEN;
 	this.setBackground(Color.GREEN);
     }
 
-    // Colour is MAGENTA, meaning it's part of solved path
+    /**
+     * If Node's colour is MAGENTA, then it is part of solved path.
+     * 
+     * @return - If Node is part of solution path.
+     */
     public boolean isPath() {
 	return colour == Color.MAGENTA;
     }
 
-    // Make Node MAGENTA, meaning it's the solved path
+    /**
+     * Makes this Node part of solution path by changing its colour to MAGENTA.
+     */
     public void makePath() {
 	colour = Color.MAGENTA;
 	this.setBackground(Color.MAGENTA);
     }
+    /* FOR PATHFINDING THROUGH THE MAP */
 
-    /****** FOR A* ALGORITHM TO UPDATE G, H, AND F COSTS ******/
-    // Resets all costs
+    /* FOR A* ALGORITHM FOR G, H, AND F COSTS */
+    /**
+     * Resets all costs of this Node back to default value of the double max value.
+     */
     public void resetCosts() {
 	gCost = Double.MAX_VALUE;
 	hCost = Double.MAX_VALUE;
 	fCost = Double.MAX_VALUE;
     }
 
+    /**
+     * Getter method to get g cost value.
+     * 
+     * @return - current g cost.
+     */
     public double getGCost() {
 	return gCost;
     }
 
+    /**
+     * Getter method to get h cost value.
+     * 
+     * @return - current h cost.
+     */
     public double getHCost() {
 	return hCost;
     }
 
+    /**
+     * Getter method to get f cost value.
+     * 
+     * @return - current f cost.
+     */
     public double getFCost() {
 	return fCost;
     }
 
+    /**
+     * Sets this Node's g cost to be the parameter value.
+     * 
+     * @param g - Updated g cost value.
+     */
     public void setGCost(double g) {
 	gCost = g;
     }
 
+    /**
+     * Sets this Node's h cost to be the parameter value.
+     * 
+     * @param g - Updated h cost value.
+     */
     public void setHCost(double h) {
 	hCost = h;
     }
 
+    /**
+     * Sets this Node's f cost to be the parameter value.
+     * 
+     * @param g - Updated f cost value.
+     */
     public void setFCost(double f) {
 	fCost = f;
     }
+    /* FOR A* ALGORITHM FOR G, H, AND F COSTS */
 
-    /****** CHANGE COLOUR OF NODE ON SPECIFIC MOUSE CLICKS ******/
+    /* ON MOUSECLICKS CHANGE THIS NODE'S COLOUR */
     @Override
     public void mouseClicked(MouseEvent e) {
 	// When mouse has been pressed and released
     }
 
+    /**
+     * If a start point hasn't been placed yet, then place that first, following an
+     * end point. After both start and end points have been placed, then all other
+     * clicks will either create a barrier / make a Node available.
+     */
     @Override
     public void mousePressed(MouseEvent e) {
 	// When mouse has just been pressed
@@ -209,10 +323,6 @@ public class Node extends JLabel implements MouseListener {
 		    this.makeAvailable();
 		}
 	    }
-	    if (e.getButton() == MouseEvent.BUTTON1) {
-		// if left click pressed
-	    }
-
 	}
     }
 
@@ -231,5 +341,6 @@ public class Node extends JLabel implements MouseListener {
     public void mouseExited(MouseEvent e) {
 	// When mouse leaves object
     }
+    /* ON MOUSECLICKS CHANGE THIS NODE'S COLOUR */
 
 }
